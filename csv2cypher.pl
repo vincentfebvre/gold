a1f92940a6704f1ec6bb720729be5666fe5546b9
Vincent Febvre <vincent.febvre@gmail.com>
2014-04-05 12:52:19 +0200

    Modify

--- csv2cypher.pl
+++ csv2cypher.pl
@@ -1,9 +1,20 @@
 use strict;
+my %h;
 
 while (<STDIN>)
 {
 	chomp;
 	my @els = split(/,/, $_);
-	print "MATCH (a { name: '${els[0]}' }), (c { name: '${els[2]}' }) CREATE UNIQUE (a)-[:${els[1]}]-(c) RETURN c;\n";
+    if (!exists($h{$els[0]}))
+    {
+        $h{$els[0]} = 1;
+        print "CREATE (a {name: '${els[0]}' }) RETURN a;\n";
+    }
+    if (!exists($h{$els[2]}))
+    {
+        $h{$els[2]} = 1;
+        print "CREATE (a {name: '${els[2]}' }) RETURN a;\n";
+    }
+    print "MATCH (a { name: '${els[0]}' }), (c { name: '${els[2]}' }) CREATE UNIQUE (a)-[:${els[1]}]-(c) RETURN a,c;\n";
 }
 
